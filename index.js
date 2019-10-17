83da0231f60578c8043ccb1fdccda324f83b519b
Kevin <kevin_huang31@yahoo.co.id>
2019-10-17 21:51:46 +0900

    updated

--- index.js
+++ index.js
@@ -1,52 +1,59 @@
-const http = require('http');
 const path = require('path');
 const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-    let filePath = path.join(
-        __dirname, 
-        'public', 
-        req.url === '/' ? 'index.html' : req.url)
-    ;
+var app = require('express')();
+var http = require('http').createServer(app);
 
-    let extname = path.extname(filePath);
-
-    let contentType = 'text/html';
-
-    switch(extname){
-        case '.js':
-            contentType = 'text/javascript';
-            break;
-        case '.js':
-            contentType = 'text/javascript';
-            break;            
-        case '.js':
-            contentType = 'text/javascript';
-            break;
-        case '.js':
-            contentType = 'text/javascript';
-            break;                
-    }
-
-    fs.readFile(filePath, (err, content) => {
-        if(err){
-            if(err.code == 'ENOENT'){
-                fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) =>{
-                    res.writeHead(200, {'Content-Type' : 'text/html'});
-                    res.end(content, 'utf8');
-                })
-            } else{
-                res.writeHead(500);
-                res.end(`Server Error: ${err.code}`);
-            }
-        } else{
-            res.writeHead(200, {'Content-Type' : contentType});
-            res.end(content, 'utf8');
-        }
-    });
-
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// const server = http.createServer((req, res) => {
+//     let filePath = path.join(
+//         __dirname, 
+//         'public', 
+//         req.url === '/' ? 'index.html' : req.url)
+//     ;
+
+//     let extname = path.extname(filePath);
+
+//     let contentType = 'text/html';
+
+//     switch(extname){
+//         case '.js':
+//             contentType = 'text/javascript';
+//             break;
+//         case '.js':
+//             contentType = 'text/javascript';
+//             break;            
+//         case '.js':
+//             contentType = 'text/javascript';
+//             break;
+//         case '.js':
+//             contentType = 'text/javascript';
+//             break;                
+//     }
+
+//     fs.readFile(filePath, (err, content) => {
+//         if(err){
+//             if(err.code == 'ENOENT'){
+//                 fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) =>{
+//                     res.writeHead(200, {'Content-Type' : 'text/html'});
+//                     res.end(content, 'utf8');
+//                 })
+//             } else{
+//                 res.writeHead(500);
+//                 res.end(`Server Error: ${err.code}`);
+//             }
+//         } else{
+//             res.writeHead(200, {'Content-Type' : contentType});
+//             res.end(content, 'utf8');
+//         }
+//     });
+
+// });
+
+// const PORT = process.env.PORT || 5000;
+
+// server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
