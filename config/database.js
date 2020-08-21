5bbd04046aec90198469a3add93970f3f5303199
Kevin <kevin_huang31@yahoo.co.id>
2020-08-21 14:06:03 +0700

    added db config

---
+++ config/database.js
@@ -0,0 +1,19 @@
+const mongoString = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.fwfxg.mongodb.net/chatroom?retryWrites=true&w=majority`;
+
+mongoose.connect(mongoString, {useNewUrlParser: true, useUnifiedTopology: true});
+
+var conn = mongoose.connection;
+
+conn.on("error", (error) => {
+    console.log(error);
+});
+
+mongoose.connection.on('connected', () => {
+    console.log("Connected to database.");
+});
+
+mongoose.connection.on('disconnected', () => {
+    console.log("Disconnected from database.");
+});
+
+module.exports = conn;
